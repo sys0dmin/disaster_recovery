1f0f9f6403257008372e75a71f1fc1ff333f9381
root <root@mon.mshome.net>
2024-04-09 13:44:57 +0000

    Finish!

--- ELK_Prometheus_recovery.sh
+++ ELK_Prometheus_recovery.sh
@@ -1,6 +1,9 @@
 # Блок восстановления ELK и Prometheus----------------------------------------------------------------------------------------------
 #Копирование конфигураций в конечные каталоги и перезапуск служб
-sudo cp -r /home/ivan/disaster_recovery/Monitoring/elasticsearch /etc;
+sudo cp -r /home/ivan/disaster_recovery/Monitoring/etc/elasticsearch /etc;
+sudo cp -r /home/ivan/disaster_recovery/Monitoring/var/elasticsearch /var/lib;
+sudo cp /home/ivan/disaster_recovery/Monitoring/logstash-nginx-es.conf /etc/logstash/conf.d
+chown -R elasticsearch:elasticsearch /var/lib/elasticsearch/;
 sudo cp /home/ivan/disaster_recovery/Monitoring/filebeat.yml /etc/filebeat/;
 sudo cp /home/ivan/disaster_recovery/Monitoring/grafana.db /var/lib/grafana;
 sudo cp /home/ivan/disaster_recovery/Monitoring/grafana.ini /etc/grafana;
